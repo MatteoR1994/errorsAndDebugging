182fb9203ff2ae313de65739cfd29acf5d5c2ed4
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-09 16:14:21 +0100

    varie

--- csv_parser/esercitazione_csvParser.js
+++ csv_parser/esercitazione_csvParser.js
@@ -45,15 +45,15 @@ try {
 
 console.log("\n\n------------------------------------------\n\n");
 
-let stringNewLine = "12; 13; 45; 23\n" +
-                    "2.1; 13; 34; 21\n" + 
-                    "3.2; 12; 4; 22\n";
+// let stringNewLine = "12; 13; 45; 23\n" +
+//                     "2.1; 13; 34; 21\n" + 
+//                     "3.2; 12; 4; 22\n";
 // let stringNewLine = "12; 13; 45,36; 23\n" +
 //                     "2,1; 13; 34; 21\n" + 
 //                     "3.2; 12; 4; 22\n";
-// let stringNewLine = "12; 13; pippo; 23\n" +
-//                     "2.1; 13; 34; 21\n" + 
-//                     "3.2; pluto; 4; 22\n";
+let stringNewLine = "12; 13; pippo; 23\n" +
+                    "2.1; 13; 34; 21\n" + 
+                    "3.2; pluto; 4; 22\n";
 // let stringNewLine = "pluto; pluto; pluto; pluto\n" +
 //                     "pluto; pluto; pluto; pluto\n" + 
 //                     "pluto; pluto; pluto; pluto\n";
@@ -64,11 +64,12 @@ let stringNewLine = "12; 13; 45; 23\n" +
 console.log("STRINGA SU PIU' LINEE\n\n");
 console.log("Stringa originale: ", stringNewLine);
 
+console.log("\nSTEP 1\n");
 let resultNewLine;
 
 try {
     resultNewLine = Parser.newLineStringParser(stringNewLine);
-    console.log("result: ", resultNewLine);
+    console.log("result step 1: ", resultNewLine);
 } catch (error) {
     if (error instanceof EmptyStringError) {
         console.log(error.message);
@@ -81,6 +82,52 @@ try {
         }
     }
 }
+
+console.log("\nSTEP 2\n");
+let resultStep2;
+
+try {
+    resultStep2 = Parser.newLineStringToArrayOfStringParser(stringNewLine);
+    console.log("result step 2: ", resultStep2);
+} catch (error) {
+    if (error instanceof EmptyStringError) {
+        console.log(error.message);
+    } else {
+        if (error instanceof PartialInvalidStringError) {
+            console.log(error.message);
+            console.log("E' stata fatto un parsing parziale: ", error.partialResult);
+        } else {
+            console.log(error.message);
+        }
+    }
+}
+
+console.log("\nSTEP 3\n");
+
+const tableString = "name; age; school; grade\n" + 
+                    "pippo; 13; colombo; 3\n" + 
+                    "pluto; 12; leopardi; 2\n" + 
+                    "paperino; 11; bertani; 2\n";
+
+let resultStep3;
+
+// try {
+//     resultStep3 = Parser.arrayOfObjectsFromStringMultiline(stringNewLine);
+//     console.log("result step 2: ", resultStep3);
+// } catch (error) {
+//     if (error instanceof EmptyStringError) {
+//         console.log(error.message);
+//     } else {
+//         if (error instanceof PartialInvalidStringError) {
+//             console.log(error.message);
+//             console.log("E' stata fatto un parsing parziale: ", error.partialResult);
+//         } else {
+//             console.log(error.message);
+//         }
+//     }
+// }
+resultStep3 = Parser.arrayOfObjectsFromStringMultiline(tableString);
+console.log("result step 3: ", resultStep3);
 
 /*
 
--- csv_parser/parser.js
+++ csv_parser/parser.js
@@ -66,4 +66,70 @@ class Parser {
         return resultArray3;
     }
 
+    static newLineStringToArrayOfStringParser(string) {
+        if (string.length === 0) {
+            throw new EmptyStringError("Stringa vuota.");
+        }
+        const arrayOfStrings = string.split(/\r?\n/);
+        arrayOfStrings.pop();
+        const finalArray = [];
+        for (const stringA of arrayOfStrings) {
+            try {
+                finalArray.push(Parser.csvParser(stringA));
+            } catch (error) {
+                if (error instanceof PartialInvalidStringError) {
+                    finalArray.push(error.partialResult);
+                } else {
+                    throw error;
+                }
+            }
+        }
+        return finalArray;
+    }
+
+    static arrayOfObjectsFromStringMultiline(string) {
+        
+        if (string.length === 0) {
+            throw new EmptyStringError("Stringa vuota.");
+        }
+
+        // Divido la stringa multilinea in un array di stringhe
+        let arrayOfStrings = string.split(/\r?\n/);
+        arrayOfStrings.pop(); // Levo l'ultimo elemento vuoto
+        
+        // Pulisco, tramite una piccola funzioncina su misura, il primo elemento dell'array di stringhe, quello dell'intestazione
+        // della tabella
+        let tableName = Parser.cleanStringAndReturnAnArray(arrayOfStrings[0]);
+        
+        const finalArray = []; // Inizializzo l'array che conterrà gli oggetti finali
+
+        // Popolo gli oggetti con la proprietà, inizialmente vuota
+        for (const element of tableName) {
+            finalArray.push({[element]: ""});
+        }
+
+        // Popolo gli oggetti
+        for (let i = 1; i < arrayOfStrings.length; i++) { // Ciclo sulle altre righe dell'array di stringhe
+            const element = Parser.cleanStringAndReturnAnArray(arrayOfStrings[i]); // Tengo la stringa corrente, pulita con la funzione su misura
+            for (const string of element) { // Ciclo ogni elemento dell'array generato dalla stringa nell'array di stringhe
+                const stringParsed = parseFloat(string); // Faccio il parse
+                if (!isNaN(stringParsed)) { // Se è un numero
+                    
+                } else {
+                    
+                }
+            }
+        }
+        
+        return finalArray;
+    }
+
+    static cleanStringAndReturnAnArray(string) {
+        let workString = string;
+        let noSpacesString = workString.replace(/ /g, "");
+        let betterDecimalNumbersString= noSpacesString.replace(/,/g, ".");
+        let stringArray = betterDecimalNumbersString.split(";");
+        return stringArray;
+    }
+
 }
